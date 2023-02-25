8cfc7567d65a7b2aab97f7ff7e9e4a58ecb37723
tavolafourcade <tavolafourcade@gmail.com>
2023-02-24 22:01:02 -0500

    [ ADD ] Cors  origin

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
-import router from './routes/auth.routes'
 import authRoutes from './routes/auth.routes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors({
+  origin: 'http://localhost:5174'
+}))
 app.use(authRoutes)
 
 export default app
